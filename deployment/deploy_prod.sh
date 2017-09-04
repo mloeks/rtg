7343809bdb607e39e1b993d7aac463fe3a027023
matze09 <matthias@loeks.net>
2017-09-04 23:21:13 +0200

    fixed issues with password-based fabric command by using -a option. this way fabric bypasses the system SSH client which somehow had problems

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source ${HOME}/dev/.pyve/rtg/bin/activate
-fab deploy_prod     ## does a remote git pull and replaces the app on the server
+fab -a --abort-on-prompts deploy_prod     ## does a remote git pull and replaces the app on the server
 deactivate

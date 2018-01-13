e49d63f78a8746293e674284e4003dc5752ce3c2
matze09 <matthias@loeks.net>
2018-01-13 16:39:52 +0100

    Configured real World Cup 2018 tournament data

--- main/management/commands/create_worldcup2018_basedata.py
+++ main/management/commands/create_worldcup2018_basedata.py
@@ -25,8 +25,18 @@ class Command(BaseCommand):
 
     def create_venues(self):
         venues = {
-            # TODO
-            # 'saint-denis': Venue(city='Saint-Denis', name='Stade de France', capacity=81338),
+            'jekaterinburg': Venue(city='Jekaterinburg', name='Zentralstadion', capacity=35696),
+            'kaliningrad': Venue(city='Kaliningrad', name='Kaliningrad-Stadion', capacity=35202),
+            'kasan': Venue(city='Kasan', name='Kasan-Arena', capacity=45015),
+            'nischni-nowgorod': Venue(city='Nischni Nowgorod', name='Stadion Nischni Nowgorod', capacity=44899),
+            'moskau-spartak': Venue(city='Moskau (Spartak)', name='Spartak-Stadion', capacity=45360),
+            'samara': Venue(city='Samara', name='Kosmos-Arena', capacity=44918),
+            'wolgograd': Venue(city='Wolgograd', name='Wolgograd Arena', capacity=45015),
+            'sankt-petersburg': Venue(city='Sankt Petersburg', name='Sankt-Petersburg-Stadion', capacity=68134),
+            'sotschi': Venue(city='Sotschi', name='Olympiastadion', capacity=44000),
+            'saransk': Venue(city='Saransk', name='Mordowia Arena', capacity=45015),
+            'rostow': Venue(city='Rostow am Don', name='Rostow Arena', capacity=45000),
+            'moskau-luschniki': Venue(city='Moskau (Luschniki)', name='Luschniki', capacity=81000),
         }
 
         for key, venue in venues.items():
@@ -35,8 +45,14 @@ class Command(BaseCommand):
 
     def create_groups(self):
         groups = {
-            # TODO
-            # 'a': TournamentGroup(name='Gruppe A', abbreviation='A'),
+            'a': TournamentGroup(name='Gruppe A', abbreviation='A'),
+            'b': TournamentGroup(name='Gruppe B', abbreviation='B'),
+            'c': TournamentGroup(name='Gruppe C', abbreviation='C'),
+            'd': TournamentGroup(name='Gruppe D', abbreviation='D'),
+            'e': TournamentGroup(name='Gruppe E', abbreviation='E'),
+            'f': TournamentGroup(name='Gruppe F', abbreviation='F'),
+            'g': TournamentGroup(name='Gruppe G', abbreviation='G'),
+            'h': TournamentGroup(name='Gruppe H', abbreviation='H'),
         }
 
         for key, group in groups.items():
@@ -49,7 +65,8 @@ class Command(BaseCommand):
             'achtel': TournamentRound(name='Achtelfinale', is_knock_out=True, display_order='020', abbreviation='AF'),
             'viertel': TournamentRound(name='Viertelfinale', is_knock_out=True, display_order='030', abbreviation='VF'),
             'halb': TournamentRound(name='Halbfinale', is_knock_out=True, display_order='040', abbreviation='HF'),
-            'finale': TournamentRound(name='Finale', is_knock_out=True, display_order='050', abbreviation='FIN'),
+            'platz3': TournamentRound(name='Spiel um Platz 3', is_knock_out=True, display_order='050', abbreviation='SP3'),
+            'finale': TournamentRound(name='Finale', is_knock_out=True, display_order='060', abbreviation='FIN'),
         }
 
         for key, round in rounds.items():
@@ -58,8 +75,45 @@ class Command(BaseCommand):
 
     def create_teams(self):
         teams = {
-            # TODO
-            # 'ALB': Team(name='Albanien', abbreviation='ALB', group=self.groups['a']),
+            'EGY': Team(name='Ägypten', abbreviation='EGY', group=self.groups['a']),
+            'RUS': Team(name='Russland', abbreviation='RUS', group=self.groups['a']),
+            'KSA': Team(name='Saudi-Arabien', abbreviation='KSA', group=self.groups['a']),
+            'URU': Team(name='Uruguay', abbreviation='URU', group=self.groups['a']),
+
+            'IRN': Team(name='Iran', abbreviation='IRN', group=self.groups['b']),
+            'MAR': Team(name='Marokko', abbreviation='MAR', group=self.groups['b']),
+            'POR': Team(name='Portugal', abbreviation='POR', group=self.groups['b']),
+            'ESP': Team(name='Spanien', abbreviation='ESP', group=self.groups['b']),
+
+            'AUS': Team(name='Australien', abbreviation='AUS', group=self.groups['c']),
+            'DEN': Team(name='Dänemark', abbreviation='DEN', group=self.groups['c']),
+            'FRA': Team(name='Frankreich', abbreviation='FRA', group=self.groups['c']),
+            'PER': Team(name='Peru', abbreviation='PER', group=self.groups['c']),
+
+            'ARG': Team(name='Argentinien', abbreviation='ARG', group=self.groups['d']),
+            'ISL': Team(name='Island', abbreviation='ISL', group=self.groups['d']),
+            'CRO': Team(name='Kroatien', abbreviation='CRO', group=self.groups['d']),
+            'NGA': Team(name='Nigeria', abbreviation='NGA', group=self.groups['d']),
+
+            'BRA': Team(name='Brasilien', abbreviation='BRA', group=self.groups['e']),
+            'CRC': Team(name='Costa Rica', abbreviation='CRC', group=self.groups['e']),
+            'SUI': Team(name='Schweiz', abbreviation='SUI', group=self.groups['e']),
+            'SRB': Team(name='Serbien', abbreviation='SRB', group=self.groups['e']),
+
+            'GER': Team(name='Deutschland', abbreviation='GER', group=self.groups['f']),
+            'MEX': Team(name='Mexiko', abbreviation='MEX', group=self.groups['f']),
+            'SWE': Team(name='Schweden', abbreviation='SWE', group=self.groups['f']),
+            'KOR': Team(name='Südkorea', abbreviation='KOR', group=self.groups['f']),
+
+            'BEL': Team(name='Belgien', abbreviation='BEL', group=self.groups['g']),
+            'ENG': Team(name='England', abbreviation='ENG', group=self.groups['g']),
+            'PAN': Team(name='Panama', abbreviation='PAN', group=self.groups['g']),
+            'TUN': Team(name='Tunesien', abbreviation='TUN', group=self.groups['g']),
+
+            'JPN': Team(name='Japan', abbreviation='JPN', group=self.groups['h']),
+            'COL': Team(name='Kolumbien', abbreviation='COL', group=self.groups['h']),
+            'POL': Team(name='Polen', abbreviation='POL', group=self.groups['h']),
+            'SEN': Team(name='Senegal', abbreviation='SEN', group=self.groups['h']),
         }
 
         for key, team in teams.items():
@@ -67,13 +121,59 @@ class Command(BaseCommand):
         return teams
 
     def create_first_round_games(self):
-        pass
-        # TODO
-        # self.a_game('FRA', 'ROU', '2016-06-10 21:00:00+02', 'saint-denis')
+        self.a_game('RUS', 'KSA', '2018-06-14 17:00:00+02', 'moskau-luschniki')
+        self.a_game('EGY', 'URU', '2018-06-15 14:00:00+02', 'jekaterinburg')
+        self.a_game('MAR', 'IRN', '2018-06-15 17:00:00+02', 'sankt-petersburg')
+        self.a_game('POR', 'ESP', '2018-06-15 20:00:00+02', 'sotschi')
+        self.a_game('FRA', 'AUS', '2018-06-16 12:00:00+02', 'kasan')
+        self.a_game('ARG', 'ISL', '2018-06-16 15:00:00+02', 'moskau-spartak')
+        self.a_game('PER', 'DEN', '2018-06-16 18:00:00+02', 'saransk')
+        self.a_game('CRO', 'NGA', '2018-06-16 21:00:00+02', 'kaliningrad')
+        self.a_game('CRC', 'SRB', '2018-06-17 14:00:00+02', 'samara')
+        self.a_game('GER', 'MEX', '2018-06-17 17:00:00+02', 'moskau-luschniki')
+        self.a_game('BRA', 'SUI', '2018-06-17 20:00:00+02', 'rostow')
+        self.a_game('SWE', 'KOR', '2018-06-18 14:00:00+02', 'nischni-nowgorod')
+        self.a_game('BEL', 'PAN', '2018-06-18 17:00:00+02', 'sotschi')
+        self.a_game('TUN', 'ENG', '2018-06-18 20:00:00+02', 'wolgograd')
+        self.a_game('COL', 'JPN', '2018-06-19 14:00:00+02', 'saransk')
+        self.a_game('POL', 'SEN', '2018-06-19 17:00:00+02', 'moskau-spartak')
 
-    def a_game(self, home, away, kickoff, venue, deadline='2016-06-10 21:00:00+02'):
+        self.a_game('RUS', 'EGY', '2018-06-19 20:00:00+02', 'sankt-petersburg')
+        self.a_game('POR', 'MAR', '2018-06-20 14:00:00+02', 'moskau-luschniki')
+        self.a_game('URU', 'KSA', '2018-06-20 17:00:00+02', 'rostow')
+        self.a_game('IRN', 'ESP', '2018-06-20 20:00:00+02', 'kasan')
+        self.a_game('DEN', 'AUS', '2018-06-21 14:00:00+02', 'samara')
+        self.a_game('FRA', 'PER', '2018-06-21 17:00:00+02', 'jekaterinburg')
+        self.a_game('ARG', 'CRO', '2018-06-21 20:00:00+02', 'nischni-nowgorod')
+        self.a_game('BRA', 'CRC', '2018-06-22 14:00:00+02', 'sankt-petersburg')
+        self.a_game('NGA', 'ISL', '2018-06-22 17:00:00+02', 'wolgograd')
+        self.a_game('SRB', 'SUI', '2018-06-22 20:00:00+02', 'kaliningrad')
+        self.a_game('BEL', 'TUN', '2018-06-23 14:00:00+02', 'moskau-spartak')
+        self.a_game('KOR', 'MEX', '2018-06-23 17:00:00+02', 'rostow')
+        self.a_game('GER', 'SWE', '2018-06-23 20:00:00+02', 'sotschi')
+        self.a_game('ENG', 'PAN', '2018-06-24 14:00:00+02', 'nischni-nowgorod')
+        self.a_game('JPN', 'SEN', '2018-06-24 17:00:00+02', 'jekaterinburg')
+        self.a_game('POL', 'COL', '2018-06-24 20:00:00+02', 'kasan')
+
+        self.a_game('URU', 'RUS', '2018-06-25 16:00:00+02', 'samara')
+        self.a_game('KSA', 'EGY', '2018-06-25 16:00:00+02', 'wolgograd')
+        self.a_game('ESP', 'MAR', '2018-06-25 20:00:00+02', 'kaliningrad')
+        self.a_game('IRN', 'POR', '2018-06-25 20:00:00+02', 'saransk')
+        self.a_game('DEN', 'FRA', '2018-06-26 16:00:00+02', 'moskau-luschniki')
+        self.a_game('AUS', 'PER', '2018-06-26 16:00:00+02', 'sotschi')
+        self.a_game('ISL', 'CRO', '2018-06-26 20:00:00+02', 'rostow')
+        self.a_game('NGA', 'ARG', '2018-06-26 20:00:00+02', 'sankt-petersburg')
+        self.a_game('MEX', 'SWE', '2018-06-27 16:00:00+02', 'jekaterinburg')
+        self.a_game('KOR', 'GER', '2018-06-27 16:00:00+02', 'kasan')
+        self.a_game('SRB', 'BRA', '2018-06-27 20:00:00+02', 'moskau-spartak')
+        self.a_game('SUI', 'CRC', '2018-06-27 20:00:00+02', 'nischni-nowgorod')
+        self.a_game('SEN', 'COL', '2018-06-28 16:00:00+02', 'samara')
+        self.a_game('JPN', 'POL', '2018-06-28 16:00:00+02', 'wolgograd')
+        self.a_game('ENG', 'BEL', '2018-06-28 20:00:00+02', 'kaliningrad')
+        self.a_game('PAN', 'TUN', '2018-06-28 20:00:00+02', 'saransk')
+
+    def a_game(self, home, away, kickoff, venue, deadline='2018-06-14 17:00:00+02'):
         hometeam = self.teams[home]
         awayteam = self.teams[away]
-        display_name = "%s - %s" % (hometeam, awayteam)
-        Game(kickoff=kickoff, deadline=deadline, round=self.rounds['vorrunde'], name=display_name,
+        Game(kickoff=kickoff, deadline=deadline, round=self.rounds['vorrunde'],
              hometeam=hometeam, awayteam=awayteam, venue=self.venues[venue]).save()

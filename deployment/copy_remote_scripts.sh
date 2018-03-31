1dcc285e9054d4931db73b4f0ccb2ba5861c7679
matze09 <matthias@loeks.net>
2018-03-31 09:58:58 +0200

    Actually overwrite config files when uploading new files
    So far the upload remote-scripts script did only copy the
    scripts to the remote ~/scripts location, but did not
    actually replace the NGINX and Django configs.

--- deployment/copy_remote_scripts.sh
+++ deployment/copy_remote_scripts.sh
@@ -2,4 +2,12 @@
 
 # copies contents of ./remote_scripts to ~/scripts/rtg on DjangoEurope server.
 
-scp -r remote_scripts/* muden@s17.wservices.ch:/home/muden/scripts/rtg/
+SSH_CON=muden@s17.wservices.ch
+
+scp -r remote_scripts/* ${SSH_CON}:/home/muden/scripts/rtg/
+
+ssh ${SSH_CON} cp -rf /home/muden/scripts/rtg/nginx/sites/* /home/muden/nginx/conf/sites
+ssh ${SSH_CON} cp -rf /home/muden/scripts/rtg/init/* /home/muden/init
+
+# remove conflicting passprotected conf, which should only overwrite PROD config in emergencies
+ssh ${SSH_CON} rm -f /home/muden/nginx/conf/sites/rtg_passprotected.conf

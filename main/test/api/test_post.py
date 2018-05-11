d5185579981c4fadd7b724a34021745a4985fc04
matze09 <matthias@loeks.net>
2018-05-11 19:29:53 +0200

    Refactor test_post a bit

--- main/test/api/test_post.py
+++ main/test/api/test_post.py
@@ -5,6 +5,7 @@ from rest_framework import status
 
 from main.models import Post
 from main.test.api.abstract_rtg_api_test import RtgApiTestCase
+from test.utils import TestModelUtils
 
 
 class PostApiTests(RtgApiTestCase):
@@ -13,38 +14,38 @@ class PostApiTests(RtgApiTestCase):
         User.objects.all().delete()
         Post.objects.all().delete()
 
-    def test_post_create_admin(self):
+    def test_create_admin(self):
         admin_user = self.create_test_user(admin=True)
         response = self.create_test_post_api(admin_user)
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(Post.objects.count(), 1)
         self.assertIsNotNone(Post.objects.get(author=admin_user))
 
-    def test_post_fail_non_admin(self):
+    def test_create_failure_non_admin(self):
         user = self.create_test_user(admin=False)
         response = self.create_test_post_api(user)
 
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
         self.assertEqual(Post.objects.count(), 0)
 
-    def delete_post_admin(self):
+    def test_delete_admin(self):
         admin_user = self.create_test_user(admin=True)
 
-        test_post = Post(title='title', content='content', author=admin_user).save()
+        test_post = TestModelUtils.create_post('content', admin_user)
         self.assertEqual(Post.objects.count(), 1)
 
         response = self.client.delete("%s%i/" % (self.POSTS_BASEURL, test_post.pk))
-        self.assertEqual(status.HTTP_204_NO_CONTENT, response)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
 
-    def delete_post_fail_non_admin(self):
+    def test_delete_failure_non_admin(self):
         user = self.create_test_user(admin=False)
 
-        test_post = Post(title='title', content='content', author=user).save()
+        test_post = TestModelUtils.create_post('content', user)
         self.assertEqual(Post.objects.count(), 1)
 
         response = self.client.delete("%s%i/" % (self.POSTS_BASEURL, test_post.pk))
-        self.assertEqual(status.HTTP_403_FORBIDDEN, response)
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
 
     def create_test_post_api(self, author):
         return self.client.post(self.POSTS_BASEURL, {'title': 'Title', 'content': 'content', 'author': author.pk},

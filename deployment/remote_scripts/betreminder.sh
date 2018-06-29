fea04c8997dbed2ec6cdb43b3c0e09a82aae5fff
matze09 <matthias@loeks.net>
2018-06-30 00:00:55 +0200

    SECRET_KEY needs to be added to the bet reminder scripts in order to have them work properly from within a crontab
    This is because the crontab does not have the normal env
    variables set.

--- deployment/remote_scripts/betreminder.sh
+++ deployment/remote_scripts/betreminder.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#export SECRET_KEY_RTG=secret
 cd ~/rtg
 ~/v/rtg/bin/python manage_prod.py betreminder
--- deployment/remote_scripts/betreminder_demo.sh
+++ deployment/remote_scripts/betreminder_demo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#export SECRET_KEY_RTG=secret
 cd ~/rtg_demo
 ~/v/rtg/bin/python manage_test.py betreminder

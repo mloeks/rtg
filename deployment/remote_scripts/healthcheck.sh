f90d1248989f9cda0fa3cd603ddd1e9630b93440
matze09 <matthias@loeks.net>
2018-08-27 21:28:28 +0200

    Changed target e-mail of healthcheck to private address.
    Since the RTG accounts are not currently working properly,
    have the healthcheck mails sent to my private e-mail account.

--- deployment/remote_scripts/healthcheck.sh
+++ deployment/remote_scripts/healthcheck.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+# TODO set back to admin@royale-tippgemeinschaft.de once account works again ... :-(
 HEALTH_STATUS=$(curl -s -o /dev/null -w "%{http_code}" 'https://api.royale-tippgemeinschaft.de/healthcheck/')
 if [[ $HEALTH_STATUS != 200 ]]; then
-    mail -s "[RTG] PROD - Service unhealthy ($HEALTH_STATUS)" admin@royale-tippgemeinschaft.de <<<\
+    mail -s "[RTG] PROD - Service unhealthy ($HEALTH_STATUS)" matthias@loeks.net <<<\
         "RTG Backend service is currently unavailable. Health endpoint returned status code $HEALTH_STATUS."
 else
     echo "Health OK."

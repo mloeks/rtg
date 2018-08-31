3ba30b771b02c285e087dcbf00172971c242aa21
matze09 <matthias@loeks.net>
2018-08-31 23:44:36 +0200

    Make sure test users are always considered "active"
    At some places in the code, we filter on only "active" users,
    which means not only those user which actually have the active
    flag set to true, but also only to those users, who have logged
    in this year.
    This was not taken care of in the test utils method which creates
    test users. This commit sets the last_login property to now()
    and therefore makes sure that test users are never filtered out.

--- main/test/api/abstract_rtg_api_test.py
+++ main/test/api/abstract_rtg_api_test.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 # TODO P3 create some more advanced tests
@@ -30,7 +32,7 @@ class RtgApiTestCase(APITestCase):
             user = User.objects.get(username=name)
             user.is_staff = admin
         except User.DoesNotExist:
-            user = User(username=name, is_staff=admin)
+            user = User(username=name, is_staff=admin, last_login=datetime.now())
 
         user.save()
 

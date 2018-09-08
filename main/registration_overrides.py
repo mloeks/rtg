63d7c7079fb5eba08245bf405bd71fdf6b14154e
matze09 <matthias@loeks.net>
2018-09-08 10:16:54 +0200

    Fix import prefix

--- main/registration_overrides.py
+++ main/registration_overrides.py
@@ -12,7 +12,7 @@ from rest_framework import serializers
 from rest_framework import status
 from rest_framework.response import Response
 
-from login_overrides import RtgObtainJSONWebToken
+from main.login_overrides import RtgObtainJSONWebToken
 from main.mail_utils import with_rtg_template
 
 

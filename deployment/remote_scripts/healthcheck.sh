8f61dc3543c300dc1a94d740925364b67484968f
Matthias Loeks <matthias.loeks@rewe-digital.com>
2020-12-14 12:49:17 +0100

    Ignore certificate errors for health endpoint
    The last auto renewal of the cert
    (on 11/12/20) was apparently not
    set such that curl sees it.
    (It's otherwise working normally).
    For now, ignore the certificate
    check using the --insecure flag.

--- deployment/remote_scripts/healthcheck.sh
+++ deployment/remote_scripts/healthcheck.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # TODO set back to admin@royale-tippgemeinschaft.de once account works again ... :-(
-HEALTH_STATUS=$(curl -s -o /dev/null -w "%{http_code}" 'https://api.royale-tippgemeinschaft.de/healthcheck/')
+# TODO remove --insecure curl flag once auto renewed certificates are properly understood by curl on the server again
+# (this broke after the automatic cert renewal which otherwise seemed to work normally on 11/12/2020)
+HEALTH_STATUS=$(curl --insecure -s -o /dev/null -w "%{http_code}" 'https://api.royale-tippgemeinschaft.de/healthcheck/')
 if [[ $HEALTH_STATUS != 200 ]]; then
     mail -s "[RTG] PROD - Service unhealthy ($HEALTH_STATUS)" matthias@loeks.net <<<\
         "RTG Backend service is currently unavailable. Health endpoint returned status code $HEALTH_STATUS."

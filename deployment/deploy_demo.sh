d9ef6d7994d039e5044640fa1b88947d2486016a
Matthias Loeks <matthias.loeks@rewe-digital.com>
2021-01-11 15:49:24 +0100

    Update deploy scripts and README to new repo URL
    After moving the repository from Bitbucket to Github, some references to
    the old repo still existed and needed to be updated.

--- deployment/deploy_demo.sh
+++ deployment/deploy_demo.sh
@@ -2,4 +2,3 @@
 
 source ${HOME}/dev/.pyve/rtg/bin/activate
 inv -e deploy-demo  # does a remote git pull and replaces the app on the server
-deactivate
--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -2,4 +2,3 @@
 
 source ${HOME}/dev/.pyve/rtg/bin/activate
 inv -e deploy-prod  # does a remote git pull and replaces the app on the server
-deactivate

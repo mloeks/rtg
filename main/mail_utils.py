1d7d54526922319f2bb3a608d887e2f5ae1e04a9
Matthias Loeks <matthias.loeks@rewe-digital.com>
2021-05-12 23:48:28 +0200

    Use new inactive_users helper method
    Sending e-mails from posts can be simplified a bit by making use of the
    new helper method. Also refactored duplicated code which makes sure only
    users which have an email (which should be all) are put into the list of
    recipients.

--- main/mail_utils.py
+++ main/mail_utils.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 from html2text import html2text
 
 from main.models import User
-from main.utils import active_users
+from main.utils import active_users, inactive_users
 
 
 def send_post_as_mail(post_instance):
@@ -12,15 +12,15 @@ def send_post_as_mail(post_instance):
     subject, from_email = settings.EMAIL_PREFIX + post_instance.title, settings.DEFAULT_FROM_EMAIL
 
     if post_instance.force_all_users:
-        target_users = User.objects.exclude(email=None).exclude(email='')
+        target_users = User.objects.all()
     elif post_instance.force_active_users:
-        target_users = active_users().exclude(email=None).exclude(email='')
+        target_users = active_users()
     elif post_instance.force_inactive_users:
-        target_users = User.objects.filter(is_active=True).filter(last_login__isnull=True)
+        target_users = inactive_users()
     else:
-        target_users = active_users().exclude(email=None).exclude(email='').filter(profile__daily_emails=True)
+        target_users = active_users().filter(profile__daily_emails=True)
 
-    recipients = list(usr.email for usr in target_users)
+    recipients = list(usr.email for usr in target_users.exclude(email=None).exclude(email=''))
     recipients.extend(list(usr.profile.email2 for usr in target_users if usr.profile.email2))
     admin_recipients = [tpl[1] for tpl in settings.ADMINS]
 
